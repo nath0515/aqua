31a18f7ed01200edb22a333b3ed6c63dd32856e1
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-22 12:10:15 +0800

    adw

---
+++ rideraccount.php
@@ -0,0 +1,238 @@
+<?php 
+    require 'session.php';
+    require 'db.php';
+
+    $user_id = $_SESSION['user_id'];
+
+    $sql = "SELECT u.user_id, username, email, role_id, firstname, lastname, address, contact_number FROM users u
+    JOIN user_details ud ON u.user_id = ud.user_id
+    WHERE u.user_id = :user_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':user_id', $user_id);
+    $stmt->execute();
+    $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (isset($_GET['start_date']) && isset($_GET['end_date'])) {
+        $start_date = $_GET['start_date'];
+        $end_date = $_GET['end_date'];
+
+        if (validateDate($start_date) && validateDate($end_date)) {
+            $start_datetime = $start_date . ' 00:00:00';
+            $end_datetime = $end_date . ' 23:59:59';
+
+            $sql = "SELECT date,expensetype_name,comment,amount FROM expense e1 JOIN expensetype e2 ON e1.expensetype_id = e2.expensetype_id WHERE date BETWEEN :start_date AND :end_date";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':start_date', $start_datetime, PDO::PARAM_STR);
+            $stmt->bindParam(':end_date', $end_datetime, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        }
+    }
+    else{
+        $sql = "SELECT date,expensetype_name,comment,amount FROM expense e1 JOIN expensetype e2 ON e1.expensetype_id = e2.expensetype_id ";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    function validateDate($date) {
+        $d = DateTime::createFromFormat('Y-m-d', $date);
+        return $d && $d->format('Y-m-d') === $date;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Orders</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.php">
+                <img src="assets/img/aquadrop.png" alt="AquaDrop Logo" style="width: 236px; height: 40px;">
+            </a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>     
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto d-flex flex-row align-items-center pe-1">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user fa-fw"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Menu</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Order Management
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="orders.php">Orders</a>
+                                    <a class="nav-link" href="stock.php">Stock</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Analytics
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="sales.php">Sales</a>
+                                <a class="nav-link" href="expenses.php">Expenses</a>
+                                    <a class="nav-link" href="#">Stock</a>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        <?php echo "".$user_data['firstname']." ".$user_data['lastname'];?>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                        
+                </main>
+            </div>
+        </div>
+
+        <!-- Edit Order Modal -->
+        <div class="modal fade" id="addexpense" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+            
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel"><i class="bi bi-plus-circle"></i> Add Expense</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="process_editorder.php" method="POST" enctype="multipart/form-data">
+                        <!-- Modal Body -->
+                        <div class="modal-body">
+                                <!-- Status -->
+                            <div class="mb-3">
+                                <label for="stock" class="form-label">Expense</label>
+                                <div class="input-group">
+                                    <?php 
+                                        $sql = "SELECT * FROM expensetype";
+                                        $stmt = $conn->prepare($sql);
+                                        $stmt->execute();
+                                        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                    ?>
+                                <select class="form-select" name="expensetype_id" required>
+                                    <option value="">Select Expense</option>
+                                    <?php foreach($data as $row):?>
+                                        <option value="<?php echo $row['expensetype_id']?>"><?php echo $row['expensetype_name']?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="commet" class="form-label">Comment</label>
+                                <div class="input-group">
+                                    <span class="input-group-text"><i class="bi bi-receipt"></i></span>
+                                    <input type="text" name="purpose" class="form-control" placeholder="Comment" required>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="commet" class="form-label">Amount</label>
+                                <div class="input-group">
+                                    <span class="input-group-text pe-3">₱</span>
+                                    <input type="number" step="0.01" name="amount" value="0.00" class="form-control" placeholder="Amount" required>
+                                </div>
+                            </div>
+                            <div class="mb-3" id="orderItemsContainer">
+                            </div>   
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success">Add Expense</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $("#editOrderBtn").click(function() {
+                    var orderId = $(this).data("id");
+
+                    $.ajax({
+                        url: "process_getorderdata.php",
+                        type: "POST",
+                        data: { order_id: orderId },
+                        dataType: "json",
+                        success: function(response) {
+                            if (response.success) {
+                                const orderItems = response.data2;
+
+                                $("#editStatusId").val(response.data.status_id);
+
+
+                                let itemsHtml = '<h5>Order Items:</h5>';
+                                orderItems.forEach(item => {
+                                    itemsHtml += `
+                                        <div>
+                                            <p>Item: ${item.product_name}</p>
+                                            <p>Quantity: ${item.quantity}</p>
+                                            <p>Price: ₱${item.price}</p>
+                                        </div>
+                                    `;
+                                });
+                                $('#orderItemsContainer').html(itemsHtml);
+                                
+                            } else {
+                                alert("Error fetching product data.");  
+                            }
+                        },
+                        error: function() {
+                            alert("Failed to fetch product details.");
+                        }
+                    });
+                });
+            });
+        </script>
+    </body>
+</html>
