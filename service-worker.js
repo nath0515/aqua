9f619891e885dc03edd063f1c235fbde5c65a595
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-24 00:51:45 +0800

    awd

--- service-worker.js
+++ service-worker.js
@@ -9,23 +9,34 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+  console.log('[ServiceWorker] Install');
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }).catch((err) => {
+      console.error('❌ Failed to cache during install:', err);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
 
 self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activate');
+
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
+            console.log('[ServiceWorker] Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
