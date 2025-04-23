92a30eab46850c0c8692c42266fe24d7995c3237
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-23 22:49:02 +0800

    awd

--- home.php
+++ home.php
@@ -169,10 +169,6 @@ require ('session.php');
             <div class="container-fluid px-4">
                 <div class="d-flex align-items-center justify-content-between small">
                     <div class="text-muted">Copyright &copy; AquaDrop 2023</div>
-                    <div>
-                        <a href="#">Privacy Policy</a> &middot;
-                        <a href="#">Terms &amp; Conditions</a>
-                    </div>
                 </div>
             </div>
         </footer>
