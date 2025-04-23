a8b835285d33c1cfee486d4f47d1c1e8a89a329a
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-24 00:41:48 +0800

    awd

--- index.php
+++ index.php
@@ -288,6 +288,9 @@
                 </footer>
             </div>
         </div>
+        <div id="loadingOverlay">
+            <div class="spinner"></div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
