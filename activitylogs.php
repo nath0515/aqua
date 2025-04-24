c39b7675c3950a66262adbae4e661edc8db899e1
Nathaiel Advento <nathaniel.advento15@gmail.com>
2025-04-25 00:15:51 +0800

    awd

--- activitylogs.php
+++ activitylogs.php
@@ -104,10 +104,10 @@
             </div>
             <div id="layoutSidenav_content">
                 <main>
-                <div class="card card-body card-dark bg-primary bubble-shadow mb-4 mt-4 animated fadeInDown m-3 ">
+                <div class="card card-body card-dark bg-primary bubble-shadow mb-4 mt-4 animated fadeInDown m-5 ">
                     <h1 class="m-2"><i class="far fa-bell"></i> &nbsp; Activity Logs</h1>
                 </div> 
-                <div class="card card-body mb-4 animated fadeInUp m-3">
+                <div class="card card-body mb-4 animated fadeInUp m-5">
                     <a class="text-dark" href="https://www.ilearnu.lu.edu.ph/student/class/7241/post/70080">
                         <div class="mx-4">
                                 Chrisna Fucio posted a new activity: CCS SUMMIT 2025
