7373eb338993c83837da1559906fbf58fe9f6768
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-28 16:54:13 +0800

    awd

--- reports.php
+++ reports.php
@@ -139,7 +139,7 @@
                             Income
                         </div>
                         <div class="card-body">
-                            <table id="datatablesSimple">
+                            <table id="datatablesSimple1">
                                 <thead>
                                     <tr>
                                         <th> Expense Name</th>
@@ -162,7 +162,7 @@
                             Income
                         </div>
                         <div class="card-body">
-                            <table id="datatablesSimple">
+                            <table id="datatablesSimple2">
                                 <thead>
                                     <tr>
                                         <th> Total Sales</th>
