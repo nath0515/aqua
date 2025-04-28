464830a3bb0f2b95346f56598f12f8635a0a4dd8
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-28 18:47:08 +0800

    awd

--- process_dailyreport.php
+++ process_dailyreport.php
@@ -1,43 +1,46 @@
 <?php 
 require ('db.php');
 require ('session.php');
-$today = date('Y-m-d');
+try {
+    $today = date('Y-m-d');
 
-$sql = "SELECT status FROM store_status WHERE ss_id = 1";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$status = $stmt->fetchColumn();
-
-if($status){
-    $sql = "UPDATE store_status SET status = 0 WHERE ss_id = 1";
-
-    $sql1 = "SELECT order_id FROM orders WHERE DATE(date) = :today";
-    $stmt = $conn->prepare($sql1);
-    $stmt->bindParam(':today', $today);
+    $sql = "SELECT status FROM store_status WHERE ss_id = 1";
+    $stmt = $conn->prepare($sql);
     $stmt->execute();
-    $orders_data = $stmt->fetchAll();
+    $status = $stmt->fetchColumn();
 
-    $sql1 = "INSERT INTO reports (date) VALUES (:today)";
-    $stmt = $conn->prepare($sql1);
-    $stmt->bindParam(':today', $today);
-    $stmt->execute();
-    $lastInsertId = $conn->lastInsertId();
+    if ($status) {
+        $sql = "UPDATE store_status SET status = 0 WHERE ss_id = 1";
 
-
-    foreach($orders_data as $row){
-        $sql2 = "INSERT INTO report_content (report_id, order_id) VALUES (:report_id, :order_id)";
-        $stmt = $conn->prepare($sql2);
-        $stmt->bindParam(':report_id', $lastInsertId);
-        $stmt->bindParam('order_id', $row['order_id']);
+        $sql1 = "SELECT order_id FROM orders WHERE DATE(date) = :today";
+        $stmt = $conn->prepare($sql1);
+        $stmt->bindParam(':today', $today);
         $stmt->execute();
+        $orders_data = $stmt->fetchAll();
+
+        $sql1 = "INSERT INTO reports (date) VALUES (:today)";
+        $stmt = $conn->prepare($sql1);
+        $stmt->bindParam(':today', $today);
+        $stmt->execute();
+        $lastInsertId = $conn->lastInsertId();
+
+        foreach ($orders_data as $row) {
+            $sql2 = "INSERT INTO report_content (report_id, order_id) VALUES (:report_id, :order_id)";
+            $stmt = $conn->prepare($sql2);
+            $stmt->bindParam(':report_id', $lastInsertId);
+            $stmt->bindParam(':order_id', $row['order_id']);
+            $stmt->execute();
+        }
+    } else {
+        $sql = "UPDATE store_status SET status = 1 WHERE ss_id = 1";
     }
 
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    header('Location: index.php');
+    exit();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-else{
-    $sql = "UPDATE store_status SET status = 1 WHERE ss_id = 1";
-}
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-header('Location: index.php'); //sa reports today papunta
-exit();
 ?>
