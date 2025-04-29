6b44991f89c86e83aa63965c0b62b4f7a72988a9
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-29 15:17:17 +0800

    adad

--- report.php
+++ report.php
@@ -163,7 +163,7 @@
         <script src="assets/demo/chart-bar-demo.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="js/datatables-simple-demo.js"></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
             $(document).ready(function() {
