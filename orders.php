93bb3946a1796b737f79733dd7637af9e77f3b03
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-29 14:17:38 +0800

    awd

--- orders.php
+++ orders.php
@@ -91,7 +91,19 @@
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="sales.php">Sales</a>
                                     <a class="nav-link" href="expenses.php">Expenses</a>
-                                    <a class="nav-link" href="#">Stock</a>
+                                    <a class="nav-link" href="income.php">Income</a>
+                                    <a class="nav-link" href="report.php">Report</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Account Management
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts2" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="accounts.php">Accounts</a>
+                                    <a class="nav-link" href="rideraccount.php">Add Rider</a>
                                 </nav>
                             </div>
                         </div>
