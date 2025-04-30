c98d37f84df11f71dc5a2cb321efd7180a0127a5
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-30 14:03:45 +0800

    awd

--- save_location.php
+++ save_location.php
@@ -8,7 +8,7 @@ try {
         $lng = floatval($_POST['lng']);
         $user_id = $_SESSION['user_id'];
 
-        $stmt = $pdo->prepare("UPDATE user_details SET latitude = :lat, longitude = :lng WHERE user_id = :user_id");
+        $stmt = $conn->prepare("UPDATE user_details SET latitude = :lat, longitude = :lng WHERE user_id = :user_id");
         $stmt->execute(['lat' => $lat, 'lng' => $lng, 'user_id' => $user_id]);
 
         echo "Success";
