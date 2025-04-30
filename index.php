58cd0caa1c518ea734b9b8c53395df0ca0ae2e7c
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-04-30 11:18:56 +0800

    awd

--- index.php
+++ index.php
@@ -210,6 +210,7 @@
                                 <div class="card bg-success text-white mb-4">
                                     <div class="card-body" style="font-size:25px">Net Income<ol class="breadcrumb">₱ <?php echo number_format(($amount2['total_sales']-$amount1['total_expense']),2); ?></ol></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="income.php">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right text-success"></i></div>
                                     </div>
                                 </div>
