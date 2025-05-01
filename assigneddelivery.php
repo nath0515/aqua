9a5f8cb800f026b0236e6b5d7aca98cac301724b
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-01 16:44:32 +0800

    awd

--- assigneddelivery.php
+++ assigneddelivery.php
@@ -81,22 +81,27 @@
                 <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
                         <div class="nav">
-                        <div class="sb-sidenav-menu-heading">Menu</div>
-                        <a class="nav-link" href="home.php">
-                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                            Dashboard
-                        </a>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                            Order Management
-                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                        </a>
-                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                            <nav class="sb-sidenav-menu-nested nav">
-                                <a class="nav-link" href="costumerorder.php">Order</a>
-                                <a class="nav-link" href="orderhistory.php">Order History</a>
-                            </nav>
-                        </div>
+                            <div class="sb-sidenav-menu-heading">Menu</div>
+                            <a class="nav-link" href="home.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Delivery Management
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="assigneddelivery.php">Assigned Deliveries</a>
+                                    <a class="nav-link" href="orderhistory.php">Delivered History</a>
+                                    <a class="nav-link" href="ridermap.php">Maps</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link" href="home.php">
+                            <div class="sb-nav-link-icon"><i class="bi bi-calendar-week"></i></i></div>
+                            Attendance
+                            </a>
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
