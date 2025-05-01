decc3bc00c4570eb12d5ae66bd7822e590621e17
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-01 21:59:38 +0800

    awd

---
+++ profile.php
@@ -0,0 +1,247 @@
+<?php 
+    require 'session.php';
+    require 'db.php';
+
+    $user_id = $_SESSION['user_id'];
+
+    $sql = "SELECT u.user_id, username, email, role_id, firstname, lastname, address, contact_number FROM users u
+    JOIN user_details ud ON u.user_id = ud.user_id
+    WHERE u.user_id = :user_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':user_id', $user_id);
+    $stmt->execute();
+    $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Orders</title>
+        <link rel="manifest" href="/manifest.json">
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.php">
+                <img src="assets/img/aquadrop.png" alt="AquaDrop Logo" style="width: 236px; height: 40px;">
+            </a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>     
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto d-flex flex-row align-items-center pe-1">
+                
+                <li class="nav-item dropdown me-1">
+                    <a class="nav-link position-relative" href="#" id="notificationDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-bell"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            3
+                            <span class="visually-hidden">unread messages</span>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="notificationDropdown">
+                        <li><a class="dropdown-item" href="#">Notification 1</a></li>
+                        <li><a class="dropdown-item" href="#">Notification 2</a></li>
+                        <li><a class="dropdown-item" href="#">Notification 3</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user fa-fw"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><a id="installBtn" class="dropdown-item" style="display: none;">Install AquaDrop</a></li>
+                        <?php 
+                        $sql = "SELECT status FROM store_status WHERE ss_id = 1";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->execute();
+                        $status = $stmt->fetchColumn();
+                        ?>
+                        <li>
+                            <a 
+                                href="process_dailyreport.php" 
+                                class="dropdown-item"
+                                <?php if ($status == 1): ?>
+                                    onclick="return confirmCloseShop(event)"
+                                <?php endif; ?>
+                            >
+                                <?php echo ($status == 1) ? 'Close Shop' : 'Open Shop'; ?>
+                            </a>
+                        </li>
+                        <div id="loadingOverlay">
+                            <div class="spinner"></div>
+                        </div>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Menu</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Order Management
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="orders.php">Orders</a>
+                                    <a class="nav-link" href="stock.php">Stock</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Analytics
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="sales.php">Sales</a>
+                                    <a class="nav-link" href="expenses.php">Expenses</a>
+                                    <a class="nav-link" href="income.php">Income</a>
+                                    <a class="nav-link" href="report.php">Report</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Account Management
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts2" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="accounts.php">Accounts</a>
+                                    <a class="nav-link" href="rideraccount.php">Add Rider</a>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        <?php echo "".$user_data['firstname']." ".$user_data['lastname'];?>
+                    </div>
+                </nav>
+            </div>
+        <div id="layoutSidenav_content">
+            <main class="container py-4">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="card shadow border-0">
+                            <div class="card-header bg-primary text-white text-center">
+                                <h3 class="mb-0">Profile</h3>
+                            </div>
+                            <div class="card-body">
+                                <form action="process_registerrider.php" method="POST" onsubmit="return checkForm()">
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">First Name</label>
+                                        <input type="text" class="form-control" id="firstname" name="firstname" required placeholder="Juan Dela Cruz">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Last Name</label>
+                                        <input type="text" class="form-control" id="lastname" name="lastname" required placeholder="Juan Dela Cruz">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="contact_number" class="form-label">Contact Number</label>
+                                        <input type="tel" class="form-control" id="contact_number" name="contact_number"
+                                            required pattern="[0-9]{11}" maxlength="11"
+                                            placeholder="09XXXXXXXXX"
+                                            oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0,11)">
+                                        <div class="invalid-feedback" id="contactError">
+                                            Please enter a valid 11-digit contact number starting with 09.
+                                        </div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="email" name="email"
+                                            placeholder="juandelacruz@gmail.com"
+                                            pattern="[a-zA-Z0-9._%+-]+@gmail\.com$"
+                                            required>
+                                        <div class="form-text">Must be a valid Gmail address (e.g., juan@gmail.com).</div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Password</label>
+                                            <div class="input-group">
+                                                <input type="password" id="password" class="form-control" name="password"  onInput="check()" placeholder="Password" required>
+                                                <span class="input-group-text" onclick="togglePassword()">
+                                                    <i class="fa fa-eye" id="eyeIcon"></i> 
+                                                </span>
+                                            </div>
+                                    </div>
+
+                                    <div class="mb-3 d-none" id="validation">
+                                            <div id="count">Length : 0</div>
+                                            <!-- Password Strength Check (Moved here under Confirm Password) -->
+                                            <div id="check0">
+                                                <i class="far fa-check-circle"></i> <span> Length more than 8.</span>
+                                            </div>
+                                            <div id="check2">
+                                                <i class="far fa-check-circle"></i> <span> Contains numerical character.</span>
+                                            </div>
+                                            <div id="check3">
+                                                <i class="far fa-check-circle"></i> <span> Contains special character.</span>
+                                            </div>
+                                            <div id="check4">
+                                                <i class="far fa-check-circle"></i> <span> Shouldn't contain spaces.</span>
+                                            </div>
+                                        </div>
+
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Confirm Password</label>
+                                        <div class="input-group">
+                                            <input type="password" id="confirm_password" class="form-control" name="confirm_password" onInput="confirmCheck()" placeholder="Password" required>
+                                            <span class="input-group-text" onclick="toggleConfirmPassword()">
+                                                <i class="fa fa-eye" id="confirmEyeIcon"></i> 
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <div class="mb-3 d-none" id="validation1">
+                                            <div id="check5">
+                                                <i class="far fa-check-circle"></i> <span> Passwords do not match.</span>
+                                            </div>       
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary w-100">Register Rider</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </body>
+</html>
