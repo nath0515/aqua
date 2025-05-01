2fb76e7dd062148e17d9f1181a60e72a5e6f1a9a
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-01 13:19:44 +0800

    awd

--- costumerorder.php
+++ costumerorder.php
@@ -128,31 +128,28 @@
 
                             <!-- Card -->
                             <?php foreach($products_data as $row):?>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-header"style="font-size: 20px">
-                                        <?php echo $row['product_name']; ?>
-                                    </div>
-                                    <div class="card-body bg-white text-center d-flex justify-content-center align-items-center" style="font-size: 25px;">
-                                        <img src="<?php echo $row['product_photo']; ?>" width="100px" height="100px" class="rounded">
-                                    </div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        Water Price: ₱<?php echo $row['water_price']; ?><br>
-                                        Container Price: ₱<?php echo $row['container_price']; ?><br>
-                                        Stock: <?php echo $row['stock']; ?>
-                                        <div class="small text-white">
-                                            <a href="#" 
-                                                class="small text-white d-inline-block editProductBtn" 
-                                                data-bs-toggle="modal" 
-                                                data-bs-target="#edititem"
-                                                data-id="<?php echo $row['product_id']; ?>">
-
-                                                <div><i class="bi bi-pencil-square"></i></div>
-                                            </a>
+                                <a href="product_details.php?id=<?php echo $row['product_id']; ?>" style="text-decoration: none; color: inherit;">
+                                    <div class="col-xl-3 col-md-6">
+                                        <div class="card bg-primary text-white mb-4">
+                                            <div class="card-header" style="font-size: 20px">
+                                                <?php echo $row['product_name']; ?>
+                                            </div>
+                                            <div class="card-body bg-white text-center d-flex justify-content-center align-items-center" style="font-size: 25px;">
+                                                <img src="<?php echo $row['product_photo']; ?>" width="100px" height="100px" class="rounded">
+                                            </div>
+                                            <div class="card-footer d-flex align-items-center justify-content-between">
+                                                <div>
+                                                    Water Price: ₱<?php echo $row['water_price']; ?><br>
+                                                    Container Price: ₱<?php echo $row['container_price']; ?><br>
+                                                    Stock: <?php echo $row['stock']; ?>
+                                                </div>
+                                                <div class="small text-white">
+                                                    <i class="bi bi-cart-plus"></i>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
+                                </a>
                             <?php endforeach;?>
                 
                         </div>
