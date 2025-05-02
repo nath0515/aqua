2bef5971176e29b3a06d7e19f2b7213de07aade8
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-02 20:10:42 +0800

    awd

--- process_editorder.php
+++ process_editorder.php
@@ -15,7 +15,7 @@
             $stmt->bindParam(':rider', $rider);
             $stmt->execute();
 
-            header('Location: riderdashboard.php?editstatus=success');
+            header('Location: orders.php?editstatus=success');
             exit();
 
         }
