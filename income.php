f1422ca3975f8f753561f87dac494f78bf357f1a
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-04 13:34:22 +0800

    awd

--- income.php
+++ income.php
@@ -15,11 +15,11 @@ ini_set('display_errors', 1);
     $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 
     $sql = "SELECT a.date, SUM(e.amount) AS total_sales, SUM(f.amount) AS total_expense, SUM(d.income) AS total_income FROM reports a
-    JOIN report_content b ON a.report_id = b.report_id
-    JOIN report_expense c ON a.report_id = c.report_id
-    JOIN report_income d ON a.report_id = d.report_id
-    JOIN orders e ON b.order_id = e.order_id
-    JOIN expense f ON  c.expense_id = f.expense_id
+    LEFT JOIN report_content b ON a.report_id = b.report_id
+    LEFT JOIN report_expense c ON a.report_id = c.report_id
+    LEFT JOIN report_income d ON a.report_id = d.report_id
+    LEFT JOIN orders e ON b.order_id = e.order_id
+    LEFT JOIN expense f ON  c.expense_id = f.expense_id
     GROUP BY a.date";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
