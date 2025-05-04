86ee605d27edf64cc1049fb8b12cc9c94a5f26b8
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-04 14:54:01 +0800

    awd

--- orders.php
+++ orders.php
@@ -66,6 +66,16 @@
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+        <style>
+            .notifcation-text{
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: block;
+                max-width: 200px;
+            }
+        </style>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
@@ -83,15 +93,7 @@
                     $stmt->execute();
                     $activity_logs = $stmt->fetchAll();
                 ?>
-                <style>
-                    .notifcation-text{
-                        white-space: nowrap;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        display: block;
-                        max-width: 200px;
-                    }
-                </style>
+                
                 <li class="nav-item dropdown me-3">
                     <a class="nav-link position-relative" href="#" id="notificationDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <i class="fas fa-bell fs-5"></i>
