70ce0edadde56b051f15d0553bc83a5148aa765d
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-05-04 13:40:14 +0800

    awd

--- process_editorder_orders.php
+++ process_editorder_orders.php
@@ -8,10 +8,17 @@
             $order_id = $_POST['order_id'];
             $rider = $_POST['rider'];
 
-            $sql = "UPDATE orders SET status_id = :status_id, rider = :rider WHERE order_id = :order_id";
+            $sql = "SELECT date FROM orders WHERE order_id = :order_id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindparam(':order_id', $order_id);
+            $stmt->execute();
+            $date = $stmt->fetchColumn();
+
+            $sql = "UPDATE orders SET status_id = :status_id, rider = :rider, date = :date WHERE order_id = :order_id";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':status_id', $status_id);
             $stmt->bindParam(':order_id', $order_id);
+            $stmt->bindParam(':date', $date);
             $stmt->bindParam(':rider', $rider);
             $stmt->execute();
 
