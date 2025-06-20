ba9e007b1ee753b01237a4e808350a8907d5eb4f
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-06-20 10:13:48 +0800

    awd

--- ridermap.php
+++ ridermap.php
@@ -373,7 +373,7 @@
             if (!currentStartMarker) {
                 const riderIcon = L.icon({
                 iconUrl: 'https://cdn-icons-png.flaticon.com/512/11431/11431942.png', // You can replace this with your preferred image
-                iconSize: [20, 20],
+                iconSize: [30, 30],
                 iconAnchor: [20, 40],
                 popupAnchor: [0, -40]
             });
