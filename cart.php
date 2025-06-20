2caf9e66637b4de0f19d926de2eb1cf0b0ab371e
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-06-20 11:08:33 +0800

    awd

--- cart.php
+++ cart.php
@@ -203,6 +203,17 @@ error_reporting(E_ALL);
                                 <!-- Payment Method -->
                                 <div class="col-md-6">
                                     <div class="form-group form-group-default">
+                                        <label>Address</label>
+                                        <select name="payment_id" id="payment_id" class="form-select">
+                                            <option value="0">Select Address</option>
+                                            <?php foreach($payment_data as $row): ?>
+                                                <option value="<?php echo $row['payment_id']?>"><?php echo $row['payment_name']?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-default">
                                         <label>Payment Method</label>
                                         <select name="payment_id" id="payment_id" class="form-select">
                                             <option value="0">Select Payment Method</option>
