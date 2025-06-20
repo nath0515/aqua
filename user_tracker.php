52bcd6fba43ec5cdc3e83a6f4e09735c97f4f4e9
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-06-20 10:45:13 +0800

    awd

--- user_tracker.php
+++ user_tracker.php
@@ -115,8 +115,7 @@
                 <main>
                     <h2 style="text-align:center;">Live Delivery Tracking</h2>
                         <div id="map"></div>
-
-                    
+                </main>   
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
