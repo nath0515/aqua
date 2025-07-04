6a8c40a5b956a707a7d9d185f9202718c27eb981
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-07-04 11:47:01 +0800

    awd

--- sales.php
+++ sales.php
@@ -247,11 +247,11 @@ ini_set('display_errors', 1);
                             <div class="d-flex align-items-end gap-3 flex-wrap mb-3">
                                 <div>
                                     <label for="start_date" class="form-label">Start Date</label>
-                                    <input type="date" id="start_date" name="start_date" class="form-control" required>
+                                    <input type="date" id="start_date" name="start_date" class="form-control" max="<?php echo date('Y-m-d'); ?>" required>
                                 </div>
                                 <div>
                                     <label for="end_date" class="form-label">End Date</label>
-                                    <input type="date" id="end_date" name="end_date" class="form-control" required>
+                                    <input type="date" id="end_date" name="end_date" class="form-control" max="<?php echo date('Y-m-d'); ?>" required>
                                 </div>
                                 <div>
                                     <label class="form-label d-block">&nbsp;</label>
