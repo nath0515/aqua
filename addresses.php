4dbfc13f195bf054116f24241589790d8cdd9a2d
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-07-23 22:38:36 +0800

    awd

--- addresses.php
+++ addresses.php
@@ -119,7 +119,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <div class="d-flex justify-content-end mb-3">
+                        <div class="d-flex justify-content mb-3">
                             <a href="mapuser.php" class="btn btn-success">
                                 <i class="fas fa-plus"></i> Add Location
                             </a>
