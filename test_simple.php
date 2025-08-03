98eeede1c690abe158ef36cfef6cbec2818f19b0
Remily Garinga <remily@360and5.com>
2025-08-04 00:49:45 +0800

    Add simple test file to check server and JavaScript functionality

---
+++ test_simple.php
@@ -0,0 +1,18 @@
+<?php
+echo "PHP is working!";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Test</title>
+</head>
+<body>
+    <h1>Simple Test Page</h1>
+    <p>If you can see this, the server is working.</p>
+    
+    <script>
+        alert('JavaScript is working!');
+        console.log('JavaScript loaded successfully!');
+    </script>
+</body>
+</html> 
