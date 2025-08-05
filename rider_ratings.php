7987f3ac1a7f2c7b4dcf082530772122cc5ec048
Remily Garinga <remily@360and5.com>
2025-08-05 20:26:38 +0800

    Add Delivery Management dropdown to My Ratings page sidebar

--- rider_ratings.php
+++ rider_ratings.php
@@ -133,6 +133,17 @@ if($total_orders > 0) {
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                            Delivery Management
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="deliveryhistory.php">Delivered History</a>
+                                <a class="nav-link" href="ridermap.php">Maps</a>
+                            </nav>
+                        </div>
                         <a class="nav-link" href="rider_ratings.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-star"></i></div>
                             My Ratings
