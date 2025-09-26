db2ef2f6fd76cf7d8f9db05adb3e1481afcc143e
nath0515 <nathaniel.advento15@gmail.com>
2025-09-26 08:18:49 +0800

    Update riderdashboard.php

--- riderdashboard.php
+++ riderdashboard.php
@@ -36,14 +36,12 @@
     $stmt->execute();
     $status_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $sql = "SELECT status FROM rider_status WHERE user_id = :user_id AND DATE(date) = :date";
+    $sql = "SELECT status FROM store_status WHERE ss_id = 1";
     $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':user_id', $user_id);
-    $stmt->bindParam(':date', $today);
     $stmt->execute();
     $rider_status = $stmt->fetchColumn();
-    if($rider_status == 0){
-        header('Location: riderdashboardclosed.php');
+    if($rider_status == 1){
+        header('Location: riderdashboard.php');
         exit();
     }
 
