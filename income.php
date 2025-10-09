bb85a39b0be2f6e4157930f0007e0021bfa76249
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-10-09 22:53:51 +0800

    asd

--- income.php
+++ income.php
@@ -189,6 +189,8 @@ ini_set('display_errors', 1);
                                     <a class="nav-link" href="accounts.php">Accounts</a>
                                     <a class="nav-link" href="rideraccount.php">Add Rider</a>
                                     <a class="nav-link" href="adminaccount.php">Add Admin</a>
+                                    <a class="nav-link" href="addstaff.php">Add Staff</a>
+                                    <a class="nav-link" href="applications.php">Applications</a>
                                 </nav>
                             </div>
                         </div>
