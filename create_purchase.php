13399ff41a333c50958dfa08458aa99db78b4b05
Exequiel Fernando <xqlfernando@gmail.com>
2025-10-14 02:39:50 +0800

    asd

--- create_purchase.php
+++ create_purchase.php
@@ -219,7 +219,7 @@
                                     <div class="mb-3 row">
                                         <div class="col">
                                             <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" id="isDiscounted" onchange="">
+                                                <input class="form-check-input" type="checkbox" id="isDiscounted" onchange="togglePriceInput()">
                                                 <label class="form-check-label" for="isDiscounted">
                                                     Discounted Price
                                                 </label>
