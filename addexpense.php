333fd218fab627145cd3570732d084aef4008011
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-11-05 16:38:39 +0800

    asd

--- addexpense.php
+++ addexpense.php
@@ -8,13 +8,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $expensetype_id = $_POST['expensetype_id'];
     $comment = $_POST['comment'];
     $amount = $_POST['amount'];
+    $date = $_POST['startDate'];
+    $time = $_POST['startTime'];
+    $datetime = (!empty($_POST['startDate']) && !empty($_POST['startTime'])) ? $_POST['startDate'] . ' ' . $_POST['startTime'] . ':00' : date('Y-m-d H:i:s');
 
     try{
-        $sql_insert = "INSERT INTO expense (expensetype_id, comment, amount) VALUES (:expensetype_id, :comment, :amount)";
+        $sql_insert = "INSERT INTO expense (expensetype_id, comment, amount, date) VALUES (:expensetype_id, :comment, :amount, :date)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindParam(':expensetype_id', $expensetype_id);
         $stmt->bindParam(':comment', $comment);
         $stmt->bindParam(':amount', $amount);
+        $stmt->bindParam(':date', $dateTime);
         $stmt->execute();
 
         header("Location: expenses.php?status=success");
