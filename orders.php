fb29d11ee65cceb29c3f6e2d7c113c46c793f63c
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-11-12 09:31:11 +0800

    awd

--- orders.php
+++ orders.php
@@ -476,7 +476,9 @@ error_reporting(E_ALL);
                                         <span class="input-group-text"><i class="bi bi-exclamation-circle-fill"></i></span>
                                         <select name="status_id" id="editStatusId" class="form-select">
                                             <?php foreach($status_data as $row):?>
-                                                <option value="<?php echo $row['status_id'] != 5 ?>"><?php echo $row['status_name']?></option>
+                                                <?php if($row['status_id'] != 5): ?> 
+                                                    <option value="<?php echo $row['status_id']?>"><?php echo $row['status_name']?></option>
+                                                <?php endif; ?>
                                             <?php endforeach;?>
                                         </select>
                                     </div>
