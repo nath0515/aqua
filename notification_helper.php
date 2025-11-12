07dfe8b528bb8e80438ea72af7de9dfe59ccdb35
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-11-12 10:26:30 +0800

    awd

--- notification_helper.php
+++ notification_helper.php
@@ -25,7 +25,7 @@ function getNotifications($conn, $user_id, $role_id) {
             $recent_sql = "SELECT * FROM activity_logs WHERE user_id = :user_id ORDER BY date DESC LIMIT 5";
         } elseif ($role_id == 3) { // Rider
             $recent_sql = "SELECT * FROM activity_logs WHERE user_id = :user_id ORDER BY date DESC LIMIT 5";
-        } elseif ($role_id == 1){ // Admin
+        } else{ // Admin
             $recent_sql = "SELECT * FROM activity_logs WHERE user_id = :user_id ORDER BY date DESC LIMIT 5";
         }
         
