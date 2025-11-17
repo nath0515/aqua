ab65c2c8d75e247586f673ac616fd8afde046ebb
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-11-18 03:35:00 +0800

    awd

--- index.php
+++ index.php
@@ -233,7 +233,7 @@ require 'db.php';
                 <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Menu</div>
-                            <a class="nav-link" href="index.php">
+                            <a class="nav-link active" href="index.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
