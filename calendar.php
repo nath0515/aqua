8450725f74b4636db89dd3c5bedf7d50e65d2da3
Remily Garinga <remily@360and5.com>
2025-11-18 19:25:10 +0800

    awd

--- calendar.php
+++ calendar.php
@@ -465,10 +465,13 @@ $current_page = basename($_SERVER['PHP_SELF']);
             background: #dc3545;
             color: white;
         }
+          .custom-navbar {
+            background: linear-gradient(135deg, #0077b6, #005a8b) !important;
+        }
     </style>
 </head>
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark custom-navbar">
          <a class="navbar-brand ps-3" href="index.php">
                 <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 170px; height: 60px;">
             </a>
