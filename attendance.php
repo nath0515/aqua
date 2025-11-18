fcd0b7b18e16dbd8f43781bb2381d4075742db56
Remily Garinga <remily@360and5.com>
2025-11-18 17:30:35 +0800

    awd

--- attendance.php
+++ attendance.php
@@ -136,7 +136,7 @@
                         <?php echo renderNotificationDropdown($notifications['recent_notifications'], $unread_count, $user_id, $role_id); ?>
                         <li><hr class="dropdown-divider"></li>
                         <li>
-                            <a class="dropdown-item text-center text-muted small" href="activitylogs.php">View all notifications</a>
+                            <a class="dropdown-item text-center text-muted small" href="activitylogsrider.php">View all notifications</a>
                         </li>
                     </ul>
                 </li>
