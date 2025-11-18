fdcd31ed553299af3fc86b1682fd3d10ca13d43f
Remily Garinga <remily@360and5.com>
2025-11-18 19:58:33 +0800

    awd

--- addresses.php
+++ addresses.php
@@ -281,10 +281,13 @@
                 0% { transform: rotate(0deg); }
                 100% { transform: rotate(360deg); }
             }
+             .custom-navbar {
+            background: linear-gradient(135deg, #0077b6, #005a8b) !important;
+        }
         </style>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark custom-navbar">
             <!-- Navbar Brand-->
            <a class="navbar-brand ps-3" href="index.php">
                 <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 170px; height: 60px;">
