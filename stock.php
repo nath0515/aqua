75431b4d88ba9e1af317610296e630b284b47c43
Remily Garinga <remily@360and5.com>
2025-11-18 15:41:59 +0800

    awd

--- stock.php
+++ stock.php
@@ -392,10 +392,6 @@
                                 </nav>
                             </div>
                     </div>
-                     <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        <?php echo "".$user_data['firstname']." ".$user_data['lastname'];?>
-                    </div>
                 </nav>
             </div>
             <div id="layoutSidenav_content">
