80f9af4dcfb00cd518d09530e64fa04a235dfcaf
Remily Garinga <remily@360and5.com>
2025-11-18 19:26:09 +0800

    awd

--- activitylogsrider.php
+++ activitylogsrider.php
@@ -100,10 +100,13 @@
                 font-weight: 600;
                 color: #000;
             }
+             .custom-navbar {
+            background: linear-gradient(135deg, #0077b6, #005a8b) !important;
+        }
     </style>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark custom-navbar">
             <!-- Navbar Brand-->
              <a class="navbar-brand ps-3" href="index.php">
                 <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 170px; height: 60px;">
