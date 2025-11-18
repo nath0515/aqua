cb39904a43068a45604a8b635faf379991fc01a5
Remily Garinga <remily@360and5.com>
2025-11-18 23:10:36 +0800

    awd

--- order_details.php
+++ order_details.php
@@ -82,9 +82,14 @@ error_reporting(E_ALL);
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+        <style>
+         .custom-navbar {
+            background: linear-gradient(135deg, #0077b6, #005a8b) !important;
+        }</style>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark custom-navbar">
             <!-- Navbar Brand-->
             <a class="navbar-brand ps-3" href="index.php">
                 <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 220px; height: 60px;">
