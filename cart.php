6bff0c6084733eaf47c7214cf10b0a326f6b44d6
Remily Garinga <remily@360and5.com>
2025-11-18 18:32:11 +0800

    awd

--- cart.php
+++ cart.php
@@ -139,7 +139,7 @@ error_reporting(E_ALL);
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
             <!-- Navbar Brand-->
            <a class="navbar-brand ps-3" href="index.php">
-                <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 220px; height: 60px;">
+                <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 170px; height: 60px;">
             </a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>     
