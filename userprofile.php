ed33159949ce224e212b9b8c0aa4468b8c021b8d
Remily Garinga <remily@360and5.com>
2025-11-18 20:00:39 +0800

    awd

--- userprofile.php
+++ userprofile.php
@@ -46,9 +46,15 @@
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+        <style>
+            .custom-navbar {
+            background: linear-gradient(135deg, #0077b6, #005a8b) !important;
+        }
+        </style>
     </head>
     <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+       <nav class="sb-topnav navbar navbar-expand navbar-dark custom-navbar">
             <a class="navbar-brand ps-3" href="index.php">
                 <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 170px; height: 60px;">
             </a>
