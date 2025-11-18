5da91405f429872ca697e729a0f1e1fbfc36a0d7
Remily Garinga <remily@360and5.com>
2025-11-18 19:03:12 +0800

    awd

--- ridermap.php
+++ ridermap.php
@@ -87,8 +87,8 @@
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.php">
-                <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 220px; height: 60px;">
+             <a class="navbar-brand ps-3" href="index.php">
+                <img src="assets/img/tagiled2.png" alt="AquaDrop Logo" style="width: 170px; height: 60px;">
             </a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>     
