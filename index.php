a8d09ae478f35089575afca83b303ac73a32ad7c
Remily Garinga <remily@360and5.com>
2025-11-18 18:00:33 +0800

    awd

--- index.php
+++ index.php
@@ -173,7 +173,7 @@ require 'db.php';
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
             <!-- Navbar Brand-->
            <a class="navbar-brand ps-3" href="index.php">
-                <img src="assets/img/tagiled4.jpg" alt="AquaDrop Logo" style="width: 140px; height: 45px;">
+                <img src="assets/img/tagiled4.jpg" alt="AquaDrop Logo" style="width: 150px; height: 50px;">
             </a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button> 
