cfde6481d51d7fea16de97da93d913f0e6031334
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-11-25 11:20:26 +0800

    awd

--- costumer_orderdetails.php
+++ costumer_orderdetails.php
@@ -445,12 +445,12 @@
                                                         <p class="text-muted"><?php echo htmlspecialchars($existing_rating['review_text']); ?></p>
                                                     </div>
                                                 <?php endif; ?>
-                                                <?php if(!empty($rating['action_taken'])): ?>
-                                                    <div class="mt-3 p-2 border rounded bg-light">
-                                                        <strong>Action Taken:</strong><br>
-                                                        <?php echo nl2br(htmlspecialchars($rating['action_taken'])); ?><br>
+                                                <?php if(!empty($existing_rating['action_taken'])): ?>
+                                                    <div class="mt-3">
+                                                        <h6>Action Taken:</h6>
+                                                        <p class="text-muted"><?php echo htmlspecialchars($existing_rating['action_taken']); ?></p>
                                                         <small class="text-muted">
-                                                            Date: <?php echo date('M d, Y H:i', strtotime($rating['action_date'])); ?>
+                                                            Date: <?php echo date('M d, Y H:i', strtotime($existing_rating['action_date'])); ?>
                                                         </small>
                                                     </div>
                                                 <?php endif; ?>
