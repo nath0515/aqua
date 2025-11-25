f693365c9a5a901d0a67774d07f24b31d2bf5921
Nathaniel Advento <nathaniel.advento15@gmail.com>
2025-11-25 08:22:48 +0800

    awed

--- index.php
+++ index.php
@@ -743,32 +743,6 @@ require 'db.php';
                 }
             });
 
-            // Bar Chart: Product Stocks
-            var barChart = document.getElementById('barChart').getContext('2d');
-            var myBarChart = new Chart(barChart, {
-                type: 'bar',
-                data: {
-                    labels: productNames,
-                    datasets: [{
-                        label: "Stocks",
-                        backgroundColor: colors,
-                        borderColor: colors,
-                        data: productStocks,
-                    }],
-                },
-                options: {
-                    responsive: true, 
-                    maintainAspectRatio: false,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    },
-                }
-            });
-
             var incomeChart = document.getElementById('incomeChart').getContext('2d');
             var myIncomeChart = new Chart(incomeChart, {
                 type: 'line',
@@ -807,6 +781,53 @@ require 'db.php';
                     },
                     layout: {
                         padding: { left: 15, right: 15, top: 15, bottom: 15 }
+                    }
+                }
+            });
+        </script>
+        <script>
+            const productNames = <?php echo json_encode($productNames); ?>;
+            const productStocks = <?php echo json_encode($productStocks); ?>;
+            const productColors = <?php echo json_encode($colors); ?>;
+
+            const ctx = document.getElementById('barChart').getContext('2d');
+
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: productNames,
+                    datasets: [{
+                        label: 'Stocks',
+                        data: productStocks,
+                        backgroundColor: productColors,
+                        borderColor: productColors,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            labels: {
+                                generateLabels: function(chart) {
+                                    return productNames.map((name, index) => {
+                                        return {
+                                            text: name,
+                                            fillStyle: productColors[index],
+                                            strokeStyle: productColors[index],
+                                            lineWidth: 2,
+                                            hidden: false,
+                                            index: index
+                                        };
+                                    });
+                                }
+                            }
+                        }
+                    },
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
                     }
                 }
             });
